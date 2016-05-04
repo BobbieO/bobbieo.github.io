10577987c01780a353d951e839cbd1b389ba5f8d
BobbieO <bobbie2504@msn.com>
2016-05-04 15:33:46 -0500

    Removed terminal "say" messages

--- highlow.php
+++ highlow.php
@@ -1,6 +1,6 @@
 <?php
 
-// `say -v Zarvox Shall, we, play, a, game`;
+
 
 // to check if arguments have been entered.
 if ($argc !== 3) {
@@ -27,7 +27,7 @@ sleep (2);
 
 // game prompts user for her guess
 fwrite(STDOUT, "I'm thinking of a number from {$min}-{$max}. What is it?\n");
-// `say -v Zarvox I am thinking of a number from 1 to 100, What is it`;
+
 
 //counts guesses
 $guessAttempts = 0;
@@ -37,21 +37,17 @@ do {
    
     if ($userGuess > $gameNum) {
         fwrite(STDOUT, "Nope, too high. Try again!\n");
-        // `say -v Hysterical Nope, too high, Try again`;
         $guessAttempts++;
     } elseif ($userGuess < $gameNum) {
         fwrite(STDOUT, "Nope, too low. Try again!\n");
-        // `say -v Hysterical Nope, too low, Try again`;
         $guessAttempts++;
     } 
 
 } while ($userGuess != $gameNum);
 
 fwrite(STDOUT, "You won! {$gameNum} was my number!\n");
-// `say -v Bad News You won, {$gameNum} was my number`;
 
 fwrite(STDOUT, "It took {$guessAttempts} times to read my mind.\n");
-// `say -v Zarvox It took {$guessAttempts} times to read my mind`;
 
 fwrite(STDOUT, "Play again?\n");
 
