fe092ffdf1b86a51ae8ddd99a0593548e5b46779
BobbieO <bobbie2504@msn.com>
2016-05-04 16:38:26 -0500

    Broke up into functions. Added user input box

--- highlow.php
+++ highlow.php
@@ -1,55 +1,63 @@
 <?php
+//require goes here when done.
+
+// function pageController()
+// {
+    $message = 'Please enter a minimum and maximum number range.';
+
+    
+    // assess and assign num values entered into form
+    function numAssess() 
+    {
+        if ($minNum < $maxNum) {
+            $min = $minNum;
+            $max = $maxNum;
+        } else if ($minNum > $maxNum) {
+            $min = $maxNum;
+            $max = $minNum;
+        } else if (!is_numeric($minNum) || !is_numeric($maxNum) ) {
+            $message = "We need a number value.\n";
+        }
+    }
 
 
 
-// to check if arguments have been entered.
-if ($argc !== 3) {
-    die ("Please enter a min-max numeric range.\n");
-}
+    function randNum()
+    {
+        //game generates random number
+        $gameNum = mt_rand ($min, $max);
+        return $gameNum;
 
-// if second number is larger
-if ($argv[1] < $argv[2]) {
-    $min = $argv[1];
-    $max = $argv[2];
-} else if ($argv[1] > $argv[2]) {
-    $min = $argv[2];
-    $max = $argv[1];
-} else if (!is_numeric($argv[1]) || !is_numeric($argv[2]) ) {
-    echo "Please enter a min-max range.\n";
-}
-
-//game generates random number
-$gameNum = mt_rand ($min, $max);
-echo "The computer is secretly thinking {$gameNum}\n";
-
-//pause for voice
-sleep (2);
-
-// game prompts user for her guess
-fwrite(STDOUT, "I'm thinking of a number from {$min}-{$max}. What is it?\n");
+        // game prompts user for her guess
+        $message = "I'm thinking of a number from {$min}-{$max}. What is it?";
+    }
 
 
-//counts guesses
-$guessAttempts = 0;
+    function userGuessing()
+    {
+        //counts guesses
+        $guessAttempts = 0;
 
-do {
-    $userGuess = trim(fgets(STDIN));
-   
-    if ($userGuess > $gameNum) {
-        fwrite(STDOUT, "Nope, too high. Try again!\n");
-        $guessAttempts++;
-    } elseif ($userGuess < $gameNum) {
-        fwrite(STDOUT, "Nope, too low. Try again!\n");
-        $guessAttempts++;
-    } 
+        do {
+            $userGuess = '';
+           
+            if ($userGuess > $gameNum) {
+                $message = "Nope, too high. Try again!";
+                $guessAttempts++;
+            } elseif ($userGuess < $gameNum) {
+                $message = "Nope, too low. Try again!";
+                $guessAttempts++;
+            } 
 
-} while ($userGuess != $gameNum);
+        } while ($userGuess != $gameNum);
 
-fwrite(STDOUT, "You won! {$gameNum} was my number!\n");
+            $message = "You won! {$gameNum} was my number!\n It took {$guessAttempts} times to read my mind.\n Play again?\n";
+    }
 
-fwrite(STDOUT, "It took {$guessAttempts} times to read my mind.\n");
 
-fwrite(STDOUT, "Play again?\n");
+// }
+
+// extract(pageController());
 
 ?>
 
@@ -60,9 +68,27 @@ fwrite(STDOUT, "Play again?\n");
 </head>
 <body>
 
-<h1></h1>
-<p>Test</p>
-<p>Test2</p>
+<h1>High-Low</h1>
+<p><?=$message;?></p>
+<form>
+    <div>
+        <input type="text" id="minNum" name="minNum" placeholder="minimum number">
+        <label for="minNum"></label>
+    
+        <input type="text" id="maxNum" name="maxNum" placeholder="maximum number">
+        <label for="midInput"></label>
+
+        <input type="submit" value="Start Game">
+    
+    </div>
+
+    <div>
+        <input type="text" id="userGuess" name="userGuess" placeholder="your guess">
+        <label for="userGuess"></label>
+
+         <input type="submit" value="My Guess">
+    </div>
+</form>
 
 
 </body>
